a1cebdca904a57fddce9539105278b44ee729910
rorosana <r.a.caraballo8@gmail.com>
2023-07-06 18:07:10 +0200

    "User.js model done"

--- models/User.js
+++ models/User.js
@@ -48,6 +48,32 @@ UserSchema.statics.findByCredentials = async function(email, password) {
     throw new Error('Invalid credentials');
 }
 
+UserSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+//before saving => hash the password
+
+UserSchema.pre('save', function(next){
+    const user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt){
+        if(err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+UserSchema.pre('remove', function(next){
+    this.model('Order').remove({owner: this._id}, next);
+})
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
