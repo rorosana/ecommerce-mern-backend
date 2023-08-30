7ae47677cfe36957ee356a70cf8da8d610e1faa3
rorosana <r.a.caraballo8@gmail.com>
2023-08-30 11:03:52 +0200

    server.js updated with products

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const io = new Server(server, {
 
 const User = require('./models/User');
 const userRoutes = require('./routes/UserRoutes');
+const productRoutes = require('./routes/ProductRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/products', productRoutes);
 
 
 server.listen(8080, ()=>{
